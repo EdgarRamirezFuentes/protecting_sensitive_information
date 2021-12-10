11b8c63ba28acb7fc9f1c65e18af2efb5246b23c
MarySalmeron <marymorrera12@gmail.com>
2021-12-09 21:14:31 -0600

    index

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['SECRET_KEY'] = "12345"
 
 @app.route("/")
 def index():
-    return render_template("./login.html")
+    return render_template("./index.html")
 
 @app.route("/login")
 def login():
